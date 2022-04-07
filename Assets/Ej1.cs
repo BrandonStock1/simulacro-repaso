9518c3ccef2deb895fa267fd5f60314971fa3866
BrandonStock1 <46919113@est.ort.edu.ar>
2022-04-07 08:36:28 -0300

    finish exercise 1 and 2

---
+++ Assets/Ej1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{
+
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Estos numeros son iguales");
+        }
+        else
+        {
+            Debug.Log("Estos numeros no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ej2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    public int prod1;
+    public int prod2;
+    public int prod3;
+    public int monto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int suma;
+        suma = (prod1 + prod2 + prod3);
+        if (monto > suma)
+        {
+            Debug.Log("El monto supera la suma de los tres productos");
+        }
+        else
+        {
+            Debug.Log("El monto es menor a la suma de los tres productos");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
